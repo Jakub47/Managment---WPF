432fab5bd41557efb00b7545cfe33586e7ec1602
Jakub47 <jakub7447@gmail.com>
2018-02-03 21:42:40 +0100

    Proba dodania do StackPanelu takiego samego dzicka

--- Zadania/Zadania/MainWindow.xaml.cs
+++ Zadania/Zadania/MainWindow.xaml.cs
@@ -23,8 +23,6 @@ namespace Zadania
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static List<Zadanie> zd = new List<Zadanie>();
-
         public MainWindow()
         {
             InitializeComponent();
@@ -40,21 +38,61 @@ namespace Zadania
 
             //List<DataRow> list = dt.AsEnumerable().ToList();
             // For each row, print the values of each column.
-            object[] take = new object[4]; int i = 0;
+
+            ListZZadaniami.ListZadan = new List<Zadanie>();
             foreach (DataRow row in dt.Rows)
             {
+                Zadanie chwilowy = new Zadanie();
                 foreach (DataColumn column in dt.Columns)
                 {
-                    if (column.ColumnName.Equals("Temat")) txtTemat.Text = row[column].ToString();
-                    if (column.ColumnName.Equals("Priorytet")) txtPriorytet.Text = row[column].ToString();
-                    if (column.ColumnName.Equals("Termin")) txtData.Text = row[column].ToString();
-                    if (column.ColumnName.Equals("Status")) txtStatus.Text = row[column].ToString();
+                    if (column.ColumnName.Equals("Temat")) chwilowy.Temat = row[column].ToString();
+                    if (column.ColumnName.Equals("Priorytet")) chwilowy.Priorytet = row[column].ToString();
+                    if (column.ColumnName.Equals("Termin")) chwilowy.Termin = DateTime.Parse(row[column].ToString());
+                    if (column.ColumnName.Equals("Status")) chwilowy.Status = row[column].ToString();
                 }
-                i = i + 1;
-                Grid another = gdZadanie;
-                another.Name = "gdZadanie" + i;
-                Zbior.Children.Add(another);
+                ListZZadaniami.ListZadan.Add(chwilowy);
             }
+
+            for(int i = 0;i<ListZZadaniami.ListZadan.Count();i++)
+            {
+                Grid dodaj = new Grid();
+                createGrid(gdZadanie, dodaj, ListZZadaniami.ListZadan.ElementAtOrDefault(i));
+            }
+                    //1) Proba
+            //Grid newOne = new Grid(); createGrid(gdZadanie, newOne);
+            //Grid w = new Grid(); createGrid(gdZadanie, w);
+            //ZbiorZadan.Children.Add(newOne);
+            //ZbiorZadan.Children.Add(w);
+                    
+                    //2)Proba
+            ///Stworz z tego metode i tworzy nowe Gridy pozniej dodawaj je do StackPanela
+            //ColumnDefinition q = new ColumnDefinition(); q.Width = GridLength.Auto;
+            //ColumnDefinition c = new ColumnDefinition(); c.Width = GridLength.Auto;
+            //Grid k = new Grid(); k.ColumnDefinitions.Add(q); k.ColumnDefinitions.Add(c);
+
+            //RowDefinition w = new RowDefinition(); w.Height = GridLength.Auto;
+            //RowDefinition h = new RowDefinition(); h.Height = GridLength.Auto;
+            //k.RowDefinitions.Add(w); k.RowDefinitions.Add(h);
+            //TextBlock s = new TextBlock(); s.Text = "as";
+            //k.Children.Add(s);
+        }
+
+        private void createGrid(Grid original,Grid clone,Zadanie element)
+        {
+            TextBlock t = new TextBlock(); t.Text = "";
+            if (!(element.Priorytet.Equals(string.Empty)))
+                t.Text += element.Temat;
+
+            if (!(element.Priorytet.Equals(string.Empty)))
+                t.Text += element.Priorytet;
+
+            if (!(element.Termin.Equals(string.Empty)))
+                t.Text += Convert.ToString(element.Termin);
+
+            if (!(element.Status.Equals(string.Empty)))
+                t.Text += element.Status;
+
+            clone.Children.Add(t);
         }
     }
 }
---
+++ Zadania/Zadania/Models/ListZZadaniami.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania.Models
+{
+    class ListZZadaniami
+    {
+        public static List<Zadanie> ListZadan { get; set; }
+    }
+}
